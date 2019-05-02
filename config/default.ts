14d70c99cf6f2cc4edf88282b5070ab4957278e2
immint023 <38607460+immint023@users.noreply.github.com>
2019-05-02 16:29:16 +0700

    Delete default.ts

--- config/default.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  HOST: 'http://localhost:',
-  PORT: 8080,
-  MONGO_URI: 'mongodb://localhost:27017/nest-practice',
-  JWT_KEY: 'supperKey',
-  SENDGRID:
-    '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',
-};
